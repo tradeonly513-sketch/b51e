d3d97bcebecec05324e48d5335429f2fdb74df49
hugocentaur-sketch <hugocentaur@gmail.com>
2025-10-03 23:39:02 +0530

    Updated HomeHeroText.jsx

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HomeHeroText = () => {
   return (
-    <div className="text-center relative depth-4 px-4 flex-1 flex items-center justify-center hero-text-cormorant">
-      <div className="w-full">
+    <div className="text-center relative depth-4 px-4 flex-1 flex items-center justify-center">
+      <div className="w-full" style={{ fontFamily: 'AlanSans, sans-serif' }}>
         <div className="text-[12vw] sm:text-[9vw] lg:text-[9.5vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[8vw] text-layer-3 mb-2 sm:mb-0">
           We keep the
         </div>
