0d1071a86c0ed737b9a649f8ae1a87a1dd466af0
yourprint699-crypto <yourprint699@gmail.com>
2025-10-03 23:05:41 +0530

    Updated HomeHeroText.jsx

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomeHeroText = () => {
   return (
-    <div className="text-center relative depth-4 px-4 flex-1 flex items-center justify-center" style={{ fontFamily: '"Canela", serif' }}>
+    <div className="text-center relative depth-4 px-4 flex-1 flex items-center justify-center" style={{ fontFamily: '"CormorantGaramond", serif' }}>
       <div className="w-full">
         <div className="text-[12vw] sm:text-[9vw] lg:text-[9.5vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[8vw] text-layer-3 mb-2 sm:mb-0">
           We keep the
