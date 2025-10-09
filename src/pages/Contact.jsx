426a651005fa6b02c33a0ddff2b04522556111eb
tradeonly513-sketch <tradeonly513@gmail.com>
2025-10-10 00:27:02 +0530

    Updated Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
 
   const socialLinks = [
     { name: 'Instagram', url: 'https://instagram.com/Amouraworks', icon: '/icons8-instagram-100.png' },
-    { name: 'Facebook', url: 'https://facebook.com/Amouraworks', icon: '/icons8-facebook-100.png' },
+    { name: 'Facebook', url: 'https://www.facebook.com/profile.php?id=61581449278227&sk=about', icon: '/icons8-facebook-100.png' },
     { name: 'LinkedIn', url: 'https://linkedin.com/company/Amouraworks', icon: '/icons8-linkedin-100.png' }
   ]
 
