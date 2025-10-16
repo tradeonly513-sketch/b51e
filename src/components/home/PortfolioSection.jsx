a699129e5d522723772858ff30c2a1123dd54ac7
tradeonly513-sketch <tradeonly513@gmail.com>
2025-10-16 18:24:02 +0530

    Updated PortfolioSection.jsx

--- src/components/home/PortfolioSection.jsx
+++ src/components/home/PortfolioSection.jsx
@@ -5,10 +5,10 @@ import { ScrollTrigger } from 'gsap/all'
 import PortfolioGallery from './PortfolioGallery'
 
 const teasers = [
-  { url: 'https://www.youtube.com/watch?v=QX08wn8Bi6E', title: 'Summer & Mason' },
+  { url: 'https://www.youtube.com/watch?v=QX08wn8Bi6E', title: 'Summer & Mason    swipe left to see more' },
   { url: 'https://www.youtube.com/watch?v=xcD17BVK0hI', title: 'Ella and Summer' },
   { url: 'https://www.youtube.com/watch?v=L9PMwOelcRk', title: 'Good' },
-  { url: 'https://www.youtube.com/watch?v=2qFnRXpSFn8', title: 'Good' },
+  { url: 'https://www.youtube.com/watch?v=2qFnRXpSFn8', title: 'Jessie & Michael' },
   { url: 'https://www.youtube.com/watch?v=j5Yo9w4QLf8', title: 'Maher and Cynthia' },
   { url: 'https://www.youtube.com/watch?v=UZrSG4UUan8', title: 'Angela and John' },
   { url: 'https://www.youtube.com/watch?v=dCbp_xoNW1s', title: 'Jessie & Michael' }
